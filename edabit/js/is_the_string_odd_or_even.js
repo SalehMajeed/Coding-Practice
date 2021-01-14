7e298adb2b81a02cfbc563bf3ae6a099b23f6847
= <saleh.majeed8@gmail.com>
2021-01-14 15:17:53 +0530

    69 questions

---
+++ edabit/js/is_the_string_odd_or_even.js
@@ -0,0 +1,6 @@
+function oddOrEven(str) {
+	return str.length % 2 == 0;
+}
+console.log(oddOrEven('apples'));
+console.log(oddOrEven('pears'));
+console.log(oddOrEven('cherry'));
---
+++ edabit/js/using_ternary_operators.js
@@ -0,0 +1,6 @@
+function ternary(b) {
+	return b ? 'yeah' : 'nope';
+}
+
+console.log(ternary(true));
+console.log(ternary(false));
