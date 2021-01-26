7d0c392d881c6398a11f40904b9da52f5d22981a
= <saleh.majeed8@gmail.com>
2021-01-26 09:23:25 +0530

    longest common prefix

--- leet_code/js/longest_common_prefix.js
+++ leet_code/js/longest_common_prefix.js
@@ -1,22 +1,25 @@
 function larget_common_prefix(strs) {
-	if (strs.length <= 0 || strs.length >= 200) return '';
-	if (strs.length == 1) return strs[0];
-	const word = strs[strs.length - 1];
-	strs.pop();
-
-	let i = 0;
-	strs.every(each_word => {
-		// if (each_word.length <= 0 || each_word.length >= 200) return false;
-		console.log(each_word.charAt(i),word[i]) 
-		i++;
-		if(each_word.length == 6) return true
-	});
-	return word.substring(0, i);
+	const word = strs.pop();
+	const count = [];
+	for (let i = 0; i < strs.length; i++) {
+		let counts = 0;
+		for (let j = 0; j < word.length; j++) {
+			if (strs[i][j] == (word[j])) {
+				counts++;
+			} else {
+				break;
+			}
+		}
+		count.push(counts);
+	}
+	return word.substring(0, Math.min(...count));
 }
-// console.log(larget_common_prefix(['flower', 'flow', 'flight']));
-// console.log(larget_common_prefix(['dog', 'racecar', 'car']));
-// console.log(larget_common_prefix(['']));
-// console.log(larget_common_prefix(['a']));
-// console.log(larget_common_prefix(['ab', 'a']));
-// console.log(larget_common_prefix(['cir', 'car']));
+console.log(larget_common_prefix(['flower', 'flow', 'flight']));
+console.log(larget_common_prefix(['dog', 'racecar', 'car']));
+console.log(larget_common_prefix(['']));
+console.log(larget_common_prefix(['a']));
+console.log(larget_common_prefix(['ab', 'a']));
+console.log(larget_common_prefix(['cir', 'car']));
 console.log(larget_common_prefix(['flower', 'flower', 'flower', 'flower']));
+console.log(larget_common_prefix(['reflower', 'flow', 'flight']));
+console.log(larget_common_prefix(['aa', 'aa']));
