cf63a09706c4d5452761c7bc4987ecf4bd311158
= <saleh.majeed8@gmail.com>
2021-04-05 01:30:42 +0530

    regex

---
+++ edabit/very_easy_algebra/count_the_syllables.js
@@ -0,0 +1,7 @@
+function count_syllables(str) {
+	return str.length / 2;
+}
+
+console.log(count_syllables('Hehehehehehe'));
+console.log(count_syllables('bobobobobobobobo'));
+console.log(count_syllables('NANANA'));
