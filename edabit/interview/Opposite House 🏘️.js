42e2d10d376f8b8597b8756af67f0d6526e223c8
= <saleh.majeed8@gmail.com>
2021-05-08 01:01:08 +0530

    medium

---
+++ edabit/interview/Opposite House 🏘️.js
@@ -0,0 +1,14 @@
+function opposite_house(house, n) {
+	return 2 * n - house + 1;
+}
+
+console.log(opposite_house(1, 3));
+console.log(opposite_house(2, 3));
+console.log(opposite_house(3, 5));
+function opposite_house(house, n) {
+	return 2 * n - house + 1;
+}
+
+console.log(opposite_house(1, 3));
+console.log(opposite_house(2, 3));
+console.log(opposite_house(3, 5));
